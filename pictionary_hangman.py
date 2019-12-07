ea86d4a808bac8c30050d3b56d0e64bb979d2f5d
mqi1 <54959087+mqi1@users.noreply.github.com>
2019-12-07 13:56:49 -0500

    lala

--- pictionary_hangman.py
+++ pictionary_hangman.py
@@ -1,15 +1,32 @@
 import random
 
-def processfile(file_link):
+def processfile():
     """
     This function processes the list of words provided by WordNet
     """
-    file = open(file_link)
+    file = open('words.txt')
     worddict = {}
     for line in file:
         line = line.split()
         worddict[line[0]] = line[1]
     return worddict
+
+
+def process_glossfile():
+    file = open('gloss.txt')
+    glossdict = {}
+    for line in file:
+        line = line.split()
+        id = line[0]
+        gloss = ' '.join(line[1:])
+        glossdict[id] = gloss
+    return glossdict
+
+
+
+
+
+
 
 
 
@@ -22,7 +39,8 @@ def processfile(file_link):
 
 
 def main():
-    print (processfile('words.txt'))
+    # print (processfile('words.txt'))
+    # print(process_glossfile())
 
 
 if __name__ == '__main__':
