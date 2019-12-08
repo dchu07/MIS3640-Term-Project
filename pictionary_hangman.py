769594cf959275c9b2f994faeea9bb6d3a334953
dchu07 <37084052+dchu07@users.noreply.github.com>
2019-12-07 21:33:15 -0500

    update

--- pictionary_hangman.py
+++ pictionary_hangman.py
@@ -30,53 +30,54 @@ def process_glossfile():
         glossdict[id] = gloss
     return glossdict
 
-
-def get_random_word(worddict):
+def retrieve_image(worddict):
     """
-    This function returns a random string from the passed list of strings.
+    This function takes a random word and test whether the word has an image then uses that word as the word for pictionary
     """
-    word_id, word = random.choice(list(worddict.items())) 
-    return word_id, word
+    data = None
+    while data is None: 
+        try:
+            word_id, word = random.choice(list(worddict.items()))
+            urls = urlopen("http://www.image-net.org/api/text/imagenet.synset.geturls?wnid={word_id}").read().decode('utf-8').split()
+            for url in urls:
+                try:
+                    data = urlopen(url).read()
+                    return word, data
+                except HTTPError as e:
+                    continue
+        # except HTTPError as e:
+        #     word_id, word = random.choice(list(worddict.items()))
+        #     return retrieve_image(word_id)
 
 
-def get_gloss(glossdict, word_id):
-    """
-    This function takes the random word chosen and returns the definition of the word from glossdict.
-    """
-    for key in glossdict:
-        if key == word_id:
-            return glossdict[key]
+# def get_random_word(worddict):
+#     """
+#     This function returns a random string from the passed list of strings.
+#     """
+#     word_id, word = random.choice(list(worddict.items())) 
+#     return word_id, word
 
 
-def retrieve_image(word_id):
-    """
-    This function takes the random word and its ID and returns the image url
-    """
-    worddict = (process_file())
-    try:
-        urls = urlopen("http://www.image-net.org/api/text/imagenet.synset.geturls?wnid={word_id}").read().decode('utf-8').split()
-        for url in urls:
-            try:
-                data = urlopen(url).read()
-                return data
-            except HTTPError as e:
-                continue
-    except HTTPError as e:
-        word_id, word = random.choice(list(worddict.items()))
-        return retrieve_image(word_id)
+# def get_gloss(glossdict, word_id):
+#     """
+#     This function takes the random word chosen and returns the definition of the word from glossdict.
+#     """
+#     for key in glossdict:
+#         if key == word_id:
+#             return glossdict[key]
 
 
-def crop_image(url):
-    image = Image.open(urllib.request.urlopen(url))
-    width, height = image.size
+# def crop_image(url):
+#     image = Image.open(urllib.request.urlopen(url))
+#     width, height = image.size
 
-    # below metrics subject to change
-    left = width/3
-    top = height/3
-    right = width/3
-    bottom = height/3
-    image_2 = image.crop((left, top, right, bottom))
-    image_2.show()
+#     # below metrics subject to change
+#     left = width/3
+#     top = height/3
+#     right = width/3
+#     bottom = height/3
+#     image_2 = image.crop((left, top, right, bottom))
+#     image_2.show()
 
 
 # HANGMANPICS = ['''
@@ -185,11 +186,16 @@ def crop_image(url):
 def main():
     worddict = (process_file())
     glossdict = (process_glossfile())
-    word_id, word = get_random_word(worddict)
-    print(word_id, word)
-    definition = get_gloss(glossdict, word_id)
-    url = retrieve_image(word_id)
-    crop_image(url)
+
+    word, url = retrieve_image(worddict)
+    print(url)
+
+    # word_id, word = get_random_word(worddict)
+    # # print(word)
+    # definition = get_gloss(glossdict, word_id)
+    # # print(definition)
+    
+    # crop_image(url)
 
     # name = input("Hello! What is your name? ")
     # print (f"Hello {name}, Welcome to Pictionary Hangman!")
